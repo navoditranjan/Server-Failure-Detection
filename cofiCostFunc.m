ec04992b65600cc19f90fb559b0f7d9d9f91b06a
navoditranjan <nrnavodit1@gmail.com>
2017-09-06 19:28:45 -0400

    First Commit

---
+++ cofiCostFunc.m
@@ -0,0 +1,43 @@
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
+                                  num_features, lambda)
+%COFICOSTFUNC Collaborative filtering cost function
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+%   num_features, lambda) returns the cost and gradient for the
+%   collaborative filtering problem.
+%
+
+
+X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), ...
+                num_users, num_features);
+
+            
+J = 0;
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
+
+
+
+
+J_temp = (X * Theta' - Y).^2;
+J = sum(sum(J_temp(R == 1)))/2 + lambda/2 .* sum(sum(Theta.^2)) + lambda/2 .* sum(sum(X.^2));
+
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda.*X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda.*Theta;
+
+
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = [X_grad(:); Theta_grad(:)];
+
+end
